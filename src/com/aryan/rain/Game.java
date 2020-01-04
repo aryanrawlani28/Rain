505a598dd74f93555c8680fc994e132ec86e7a1f
Aryan Rawlani <aryanrawlani007@gmail.com>
2020-01-04 16:36:32 +0530

    Window setup and added components

--- src/com/aryan/rain/Game.java
+++ src/com/aryan/rain/Game.java
@@ -1,14 +1,26 @@
 package com.aryan.rain;
 
-public class Game implements Runnable{
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable{
 
     public static int width = 300;
-    public static int height = width*16/9;
+    public static int height = width / 16 * 9;
     public static int scale = 3;
+
+    private JFrame frame;
 
     private Thread thread;
 
     private boolean running = false;
+
+    public Game(){
+        Dimension size = new Dimension(width*scale, height*scale);
+        setPreferredSize(size);
+
+        frame = new JFrame();
+    }
 
     private synchronized void start(){
         running = true;
@@ -25,10 +37,28 @@ public class Game implements Runnable{
         }
     }
 
-    void run(){
+    public void run(){
         while (running){
             // game elements
+            System.out.println("Running the game!");
         }
+    }
+
+    public static void main(String[] args){
+        Game game = new Game();
+
+        game.frame.setResizable(false);
+        game.frame.setTitle("Rain");
+        game.frame.add(game);
+
+        game.frame.pack();
+        game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.frame.setLocationRelativeTo(null);
+        game.frame.setVisible(true);
+        // game.frame.setPreferredSize();
+
+        game.start();
+
     }
 
 }
