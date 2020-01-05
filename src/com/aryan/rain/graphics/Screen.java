b8f47ddc4cb14c3890b49c1b040e84ffd5059c5d
Aryan Rawlani <aryanrawlani007@gmail.com>
2020-01-05 17:09:53 +0530

    Generated tiles!

--- src/com/aryan/rain/graphics/Screen.java
+++ src/com/aryan/rain/graphics/Screen.java
@@ -1,16 +1,25 @@
 package com.aryan.rain.graphics;
 
+import java.util.Random;
+
 public class Screen {
     private int width;
     private int height;
 
     public int[] pixels;
 
+    public int[] tiles;
+    public Random random = new Random();
+
     public Screen(int width, int height){
         this.width = width;
         this.height = height;
 
         pixels = new int[width*height]; // 50,400 Size [0, 50399]
+        tiles = new int[64*64];
+        for(int i=0; i<tiles.length; i++){
+            tiles[i] = random.nextInt(0xffffff);
+        }
     }
 
     public void clear(){
@@ -20,12 +29,13 @@ public class Screen {
     }
 
     public void render(){
-
+        int tileIndex;
         for(int y=0; y < height; y++){
             if(y < 0 || y > height) break;
             for(int x=0; x < width; x++){
                 if(x < 0 || x > width) break;
-                pixels[x + y * width] = 0xff00ff; //0b10111 - binary
+                tileIndex = (x >> 4) + (y >> 4) * 64;
+                pixels[x + y * width] = tiles[tileIndex];
             }
         }
     }
