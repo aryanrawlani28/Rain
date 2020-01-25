081b26f7427f592eefea51d349d8c0053b388ae8
Aryan Rawlani <aryanrawlani007@gmail.com>
2020-01-25 20:53:01 +0530

    Move method added #42

--- src/com/aryan/rain/entity/mob/Mob.java
+++ src/com/aryan/rain/entity/mob/Mob.java
@@ -5,11 +5,24 @@ import com.aryan.rain.graphics.Sprite;
 
 public class Mob extends Entity {
     protected Sprite sprite;
-    protected int dir = 0;      // 0 -> north. 1 -> east, 2 -> south, 3 -> west.
+    protected int dir = 0;              // 0 -> north. 1 -> east, 2 -> south, 3 -> west. Clockwise.
     protected boolean moving = false;   // Mobs usually have moving animations
 
-    public void move(){
+    public void move(int xa, int ya){
+        // Any mob: xa -> where moves on x axis, ya -> where move on y axis.
+        // Have 3 states: Not moving, up/down, left/right (individually)
 
+        // 0 -> north. 1 -> east, 2 -> south, 3 -> west. Clockwise.
+        if (xa > 0) dir = 1;
+        if (xa < 0) dir = 3;
+        if (ya > 0) dir = 2;
+        if (ya < 0) dir = 0;
+
+        // If no collision -> can move.
+        if(!collision()) {
+            x += xa;
+            y += ya;
+        }
     }
 
     public void update(){
