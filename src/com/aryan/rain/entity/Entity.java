9bdabf39a589e6c37a01cf5f8899869a122e1364
Aryan Rawlani <aryanrawlani007@gmail.com>
2020-01-25 17:10:15 +0530

    Entity framework #39

---
+++ src/com/aryan/rain/entity/Entity.java
@@ -0,0 +1,30 @@
+package com.aryan.rain.entity;
+
+import com.aryan.rain.graphics.Screen;
+import com.aryan.rain.level.Level;
+
+import java.util.Random;
+
+public abstract class Entity {
+    public int x, y;
+    private boolean removed = false;
+    protected Level level;
+    protected final Random random = new Random();   // AI and stuff.
+
+    public void update(){
+
+    }
+
+    public void render(Screen screen){
+
+    }
+
+    // Remove entity from level
+    public void remove(){
+        removed = true;
+    }
+
+    public boolean isRemoved(){
+        return removed;
+    }
+}
