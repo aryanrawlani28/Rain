0469107a28e3e0a62f5050ec7668237263fa076b
Aryan Rawlani <aryanrawlani007@gmail.com>
2020-01-25 23:51:04 +0530

    Sprites are animated! #50

--- src/com/aryan/rain/entity/mob/Player.java
+++ src/com/aryan/rain/entity/mob/Player.java
@@ -8,10 +8,12 @@ public class Player extends Mob {
 
     private Keyboard input;
     private Sprite sprite;
+    private int anim = 0;
+    private boolean walking = false;
 
     public Player(Keyboard input){
         this.input = input;
-        sprite = Sprite.player_back;
+        sprite = Sprite.player_forward;
     }
 
     // Sometimes players are created at a specific location.
@@ -27,6 +29,11 @@ public class Player extends Mob {
         // When press keys, move our player from here. Affects the entity x and y.
         // If user presses up+down at same time, this will effectively cancel movement and player will not move.
         int xa = 0, ya = 0;
+        if (anim < 7500){
+            anim++;
+        }else {
+            anim = 0;
+        }
         if (input.up) ya--;
         if (input.down) ya++;
         if (input.left) xa--;
@@ -34,18 +41,58 @@ public class Player extends Mob {
 
         if (xa != 0 || ya != 0){
             move(xa, ya);
+            walking = true;
+        }else {
+            walking = false;
         }
     }
 
     // You don't wanna center it always to the player.
     public void render(Screen screen){
         int flip = 0;
-        if (dir == 0) sprite = Sprite.player_forward;
-        if(dir == 1) sprite = Sprite.player_side;
-        if (dir == 2) sprite = Sprite.player_back;
+        if (dir == 0){
+            sprite = Sprite.player_forward;
+            if (walking){
+                if (anim % 20 > 10){
+                    sprite = Sprite.player_forward_1;
+                }else{
+                    sprite = Sprite.player_forward_2;
+                }
+            }
+        }
+
+        if(dir == 1){
+            sprite = Sprite.player_side;
+            if (walking){
+                if (anim % 20 > 10){
+                    sprite = Sprite.player_side_1;
+                }else{
+                    sprite = Sprite.player_side_2;
+                }
+            }
+        }
+
+        if (dir == 2) {
+            sprite = Sprite.player_back;
+            if (walking){
+                if (anim % 20 > 10){
+                    sprite = Sprite.player_back_1;
+                }else{
+                    sprite = Sprite.player_back_2;
+                }
+            }
+        }
+
         if (dir == 3) {
             sprite = Sprite.player_side;
             flip = 1;
+            if (walking){
+                if (anim % 20 > 10){
+                    sprite = Sprite.player_side_1;
+                }else{
+                    sprite = Sprite.player_side_2;
+                }
+            }
         }
 
         screen.renderPlayer(x-16, y-16, sprite, flip);
--- src/com/aryan/rain/graphics/Sprite.java
+++ src/com/aryan/rain/graphics/Sprite.java
@@ -17,10 +17,19 @@ public class Sprite {
     public static Sprite voidTile = new Sprite(16, 0x339BF5);       // 0 is black. 0xffffff is white.
 
 
-    public static Sprite player_forward = new Sprite(32, 0, 5, SpriteSheet.tiles);
-    public static Sprite player_back = new Sprite(32, 2, 5, SpriteSheet.tiles);
-
+    public static Sprite player_back = new Sprite(32, 0, 5, SpriteSheet.tiles);
+    public static Sprite player_forward = new Sprite(32, 2, 5, SpriteSheet.tiles);
     public static Sprite player_side = new Sprite(32, 1, 5, SpriteSheet.tiles);
+
+    public static Sprite player_forward_1 = new Sprite(32, 0, 6, SpriteSheet.tiles);
+    public static Sprite player_forward_2 = new Sprite(32, 0, 7, SpriteSheet.tiles);
+
+    public static Sprite player_side_1 = new Sprite(32, 1, 6, SpriteSheet.tiles);
+    public static Sprite player_side_2 = new Sprite(32, 1, 7, SpriteSheet.tiles);
+
+    public static Sprite player_back_1 = new Sprite(32, 2, 6, SpriteSheet.tiles);
+    public static Sprite player_back_2 = new Sprite(32, 2, 7, SpriteSheet.tiles);
+
 
     // Even tho we have 16px sprites, monsters, etc can be big. So they can be bigger.
     public Sprite(int size, int x, int y, SpriteSheet sheet){
