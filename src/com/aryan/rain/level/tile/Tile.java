f1c07bfa14e61104587a125796adabdfc29eda85
Aryan Rawlani <aryanrawlani007@gmail.com>
2020-01-26 17:42:25 +0530

    Spawn level framework and tiles completed #59

--- src/com/aryan/rain/level/tile/Tile.java
+++ src/com/aryan/rain/level/tile/Tile.java
@@ -4,6 +4,7 @@ package com.aryan.rain.level.tile;
 
 import com.aryan.rain.graphics.Screen;
 import com.aryan.rain.graphics.Sprite;
+import com.aryan.rain.level.tile.spawn_level.*;
 
 // Will be a template aka abstract in future maybe.
 public class Tile {
@@ -17,6 +18,16 @@ public class Tile {
     public static Tile flower = new FlowerTile(Sprite.flower);
     public static Tile rock = new RockTile(Sprite.rock);
 
+
+    ////////////// Spawn Tiles ////////////////
+
+    // Each has own class because can have diff way of behaving, eg: one is breakable, another is passable, etc.
+    public static Tile spawn_grass = new SpawnGrassTile(Sprite.spawn_grass);
+    public static Tile spawn_hedge = new SpawnHedgeTile(Sprite.spawn_hedge);
+    public static Tile spawn_water = new SpawnWaterTile(Sprite.spawn_water);
+    public static Tile spawn_wall1 = new SpawnWallTile(Sprite.spawn_wall1);
+    public static Tile spawn_wall2 = new SpawnWallTile(Sprite.spawn_wall2); // If just for aesthetic purpose, can have same class.
+    public static Tile spawn_floor = new SpawnFloorTile(Sprite.spawn_floor);
 
     public static Tile voidTile = new VoidTile(Sprite.voidTile);
 
