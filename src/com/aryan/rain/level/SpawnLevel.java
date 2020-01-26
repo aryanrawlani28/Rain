43bbefadf5c0744da8a30f12dbc6f481e50de502
Aryan Rawlani <aryanrawlani007@gmail.com>
2020-01-26 15:54:54 +0530

    Pixels to tiles, new way of generating a level #53

--- src/com/aryan/rain/level/SpawnLevel.java
+++ src/com/aryan/rain/level/SpawnLevel.java
@@ -1,5 +1,6 @@
 package com.aryan.rain.level;
 
+import com.aryan.rain.level.tile.GrassTile;
 import com.aryan.rain.level.tile.Tile;
 
 import javax.imageio.ImageIO;
@@ -21,6 +22,7 @@ public class SpawnLevel extends Level{
             BufferedImage image = ImageIO.read(SpawnLevel.class.getResource(path));
             int w = image.getWidth();
             int h = image.getHeight();
+            tiles = new Tile[w * h];
             image.getRGB(0,0,w,h,levelPixels,0,w);
         }catch (IOException e){
             System.out.println("Rain couldn't load level file.");
@@ -29,9 +31,17 @@ public class SpawnLevel extends Level{
     }
 
     // Convert levelpixels[] to tiles.
-    protected void generateLevel(){
+    protected void generateLevel(){ // void return because it will be faster.
+        // There are as many pixels as there are tiles.
 
-
+        // Green -> Grass [0xFF00]
+        // Yellow -> Flower [0xFFFF00]
+        // Brown -> Rock [0x7F7F00]
+        for (int i = 0; i < levelPixels.length; i++) {
+            if (levelPixels[i] == 0xFF00) tiles[i] = Tile.grass;
+            if (levelPixels[i] == 0xFFFF00) tiles[i] = Tile.flower;
+            if (levelPixels[i] == 0x7F7F00) tiles[i] = Tile.rock;
+        }
     }
 
 }
