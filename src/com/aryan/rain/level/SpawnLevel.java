9c1a3ab34f51e676c27d332ae4e64ec01cc3b198
Aryan Rawlani <aryanrawlani007@gmail.com>
2020-02-04 20:39:50 +0530

    Different types of players are now available upon spawn #124

--- src/com/aryan/rain/level/SpawnLevel.java
+++ src/com/aryan/rain/level/SpawnLevel.java
@@ -35,16 +35,16 @@ public class SpawnLevel extends Level{
         }
 
         for (int i=0; i < Dummy.NO_OF_DUMMIES; i++) {
-            //add(new Dummy(random.nextInt(2) + 20, random.nextInt(7) + 59));
+            add(new Dummy(random.nextInt(2) + 20, random.nextInt(7) + 59));     // Adds dummies in a small area.
             //add(new Dummy(20, 59));
         }
 
         for (int i=0; i < Chaser.NO_OF_CHASERS; i++) {
-            //add(new Chaser(20, 59));
+            add(new Chaser(20, 59));
         }
 
         for (int i = 0; i < Star.NO_OF_STAR; i++) {
-            //add(new Star(17, 35));
+            add(new Star(17, 35));
         }
 
         for (int i = 0; i < Shooter.NO_OF_SHOOTER; i++) {
